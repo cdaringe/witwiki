9d18c317b28c206efa4599d6b7475546581d0f90
cdaringe <cdaringe@gmail.com>
2023-01-29 12:41:01 -0800

    refactor int => integer sqlite decls

--- witwiki_migrate/db/migrations/0001.sql
+++ witwiki_migrate/db/migrations/0001.sql
@@ -4,14 +4,14 @@ create table if not exists user (
     first_name varchar(50),
     last_name varchar(50),
     user_preferences_id int,
-    authentication_strategy int not null default 1
+    authentication_strategy integer not null default 1
 );
 -- migration
 create table if not exists identity_authentication_strategy_unpw (
   id integer not null primary key autoincrement,
   salt varchar(200) not null,
   hash varchar(200) not null,
-  user_id int not null unique,
+  user_id integer not null unique,
   foreign key (user_id) references user (id)
 );
 -- migration
@@ -22,18 +22,18 @@ create table if not exists user_preference (
 -- migration
 create table if not exists post (
   id integer not null primary key autoincrement,
-  user_id int not null,
+  user_id integer not null,
   body text not null,
   title varchar(300) not null,
   slug text not null unique,
-  created_at int not null default (strftime('%s','now')),
-  updated_at int not null default (strftime('%s','now'))
+  created_at integer not null default (strftime('%s','now')),
+  updated_at integer not null default (strftime('%s','now'))
 );
 -- migration
 create table if not exists post_tag (
   id integer not null primary key autoincrement,
-  post_id int not null,
-  tag_id int not null,
+  post_id integer not null,
+  tag_id integer not null,
   foreign key (post_id) references post (id),
   foreign key (tag_id) references tag (id),
   unique (post_id, tag_id)
@@ -42,28 +42,28 @@ create table if not exists post_tag (
 create index if not exists slug_idx ON post (slug);
 -- migration
 create table if not exists post_history (
-  id integer primary key autoincrement,
-  user_id int not null,
-  post_id int not null,
-  body_revision int not null,
-  title_revision int not null
+  id integer not null primary key autoincrement,
+  user_id integer not null,
+  post_id integer not null,
+  body_revision integer not null,
+  title_revision integer not null
 );
 -- migration
 create table if not exists post_body_revision_diff (
-  id integer primary key autoincrement,
-  revision_id int not null,
+  id integer not null primary key autoincrement,
+  revision_id integer not null,
   rev_type text not null,
   rev_text text default null,
   foreign key (revision_id) references post_history (id)
 );
 -- migration
 create table if not exists media_type (
-  id integer primary key autoincrement,
+  id integer not null primary key autoincrement,
   description varchar(20) not null unique
 );
 -- migration
 create table if not exists tag (
-  id integer primary key autoincrement,
+  id integer not null primary key autoincrement,
   media_type_id integer not null,
   tag varchar(30) not null,
   foreign key (media_type_id) references media_type (id),
@@ -71,15 +71,16 @@ create table if not exists tag (
 );
 -- migration
 create table if not exists post_comment (
-  id integer primary key autoincrement,
-  post_id int not null,
+  id integer not null primary key autoincrement,
+  post_id integer not null,
   body text not null,
-  user_id int not null,
-  created_at int not null default (strftime('%s','now')),
+  user_id integer not null,
+  created_at integer not null default (strftime('%s','now')),
   foreign key (post_id) references post (id),
   foreign key (user_id) references user (id),
   unique (user_id, created_at)
 );
 create table if not exists meta (
-  version int not null
+  id integer not null primary key autoincrement,
+  version integer not null
 )
